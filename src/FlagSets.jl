45cd1e63a2c43feaf7b97726a803080f40466f3c
Jéssica Milaré <jessymilare@gmail.com>
2020-10-20 17:21:33 -0300

    First commit.

---
+++ src/FlagSets.jl
@@ -0,0 +1,315 @@
+# This file is part of FlagSets.jl project, which is licensed under BDS-3-Clause.
+# See file LICENSE.md for more information.
+
+module FlagSets
+
+import Core.Intrinsics.bitcast
+export FlagSet, @flagset, flags
+
+function flagnames end
+function flags end
+function namemap end
+
+abstract type FlagSet{T<:Integer} <: AbstractSet{Symbol} end
+
+basetype(::Type{<:FlagSet{T}}) where {T<:Integer} = T
+
+# Bits manipulation
+(::Type{I})(x::FlagSet{T}) where {I<:Integer,T<:Integer} = I(bitcast(T, x))::I
+Base.cconvert(::Type{I}, x::FlagSet{T}) where {I<:Integer,T<:Integer} = I(x)
+Base.write(io::IO, x::FlagSet{T}) where {T<:Integer} = write(io, T(x))
+Base.read(io::IO, ::Type{T}) where {T<:FlagSet} = T(read(io, basetype(T)))
+#Base.isless(x::FlagSet{T}, y::FlagSet{T}) where {T<:Integer} = isless(T(x), T(y))
+Base.:|(x::T, y::T) where {T<:FlagSet} = T(basetype(T)(x) | basetype(T)(y))
+Base.:&(x::T, y::T) where {T<:FlagSet} = T(basetype(T)(x) & basetype(T)(y))
+Base.:⊻(x::T, y::T) where {T<:FlagSet} = T(basetype(T)(x) ⊻ basetype(T)(y))
+Base.:~(x::T) where {T<:FlagSet} = T(~basetype(T)(x))
+
+# Iterator interface
+function Base.iterate(x::FlagSet{T}) where {T<:Integer}
+    xi = T(x)
+    iterate(x, xi)
+end
+
+function Base.iterate(x::T, xi::Integer) where {T<:FlagSet}
+    iszero(xi) && (return nothing)
+    fs = flagnames(T)
+    nbit = trailing_zeros(xi)
+    xi ⊻= 1 << nbit
+    return (fs[nbit+1], xi)
+end
+
+function Base.first(x::T) where {T<:FlagSet}
+    xi = basetype(T)(x)
+    iszero(xi) && throw(ArgumentError("collection must be non-empty"))
+    fs = flagnames(T)
+    nbit = trailing_zeros(xi)
+    fs[nbit+1]
+end
+
+Base.length(x::FlagSet{T}) where {T<:Integer} = count_ones(T(x))
+Base.isempty(x::FlagSet{T}) where {T<:Integer} = iszero(T(x))
+
+"""
+    flags(x::FlagSet)
+
+Return the set of flags in `x` as a `Tuple`.
+
+# Examples
+```jldoctest fontflags
+julia> @flagset FontFlags bold=1 italic=2 large=4
+
+julia> flags(FontFlags(3))
+(:bold, :italic)
+```
+
+"""
+flags(x::FlagSet) = NTuple{length(x),Symbol}(x)
+
+# Set manipulation
+Base.union(x::T, y::T) where {T<:FlagSet} = T(basetype(T)(x) | basetype(T)(y))
+Base.intersect(x::T, y::T) where {T<:FlagSet} = T(basetype(T)(x) & basetype(T)(y))
+Base.setdiff(x::T, y::T) where {T<:FlagSet} = T(basetype(T)(x) & ~basetype(T)(y))
+Base.issubset(x::FlagSet{T}, y::FlagSet{T}) where {T<:Integer} = (T(x) & T(y)) == T(x)
+Base.in(elt::Symbol, x::T) where {T<:FlagSet} =
+    !iszero(get(namemap(T), elt, 0) & basetype(T)(x))
+Base.:⊊(x::FlagSet{T}, y::FlagSet{T}) where {T<:Integer} = x != y && (T(x) & T(y)) == T(x)
+
+Base.empty(s::T, ::Type{Symbol} = Symbol) where {T<:FlagSet} = T()
+
+# Printing
+function Base.print(io::IO, x::FlagSet)
+    compact = get(io, :compact, false)::Bool
+    xi = Integer(x)
+    print(io, typeof(x))
+    if compact
+        print(io, "(", xi, ")")
+    else
+        len = length(x)
+        xt = flags(x)
+        if len == 1
+            print(io, "(")
+            show(io, xt[1])
+            print(io, ")")
+        else
+            print(io, xt)
+        end
+    end
+end
+
+function Base.show(io::IO, x::FlagSet)
+    compact = get(io, :compact, false)::Bool
+    xi = Integer(x)
+    Base.show_datatype(io, typeof(x))
+    if compact
+        print(io, "(")
+        show(io, xi)
+        print(io, ")")
+    else
+        len = length(x)
+        xt = flags(x)
+        if len == 1
+            print(io, "(")
+            show(io, xt[1])
+            print(io, ")")
+        else
+            print(io, xt)
+        end
+    end
+end
+
+function Base.show(io::IO, ::MIME"text/plain", x::FlagSet)
+    show(io, x)
+    if !get(io, :compact, false)
+        print(io, " = ")
+        show(io, Integer(x))
+    end
+end
+
+# Printing FlagSet type
+function Base.show(io::IO, m::MIME"text/plain", t::Type{<:FlagSet})
+    if isconcretetype(t)
+        print(io, "FlagSet ")
+        Base.show_datatype(io, t)
+        print(io, ":")
+        xi = one(basetype(t))
+        for sym ∈ flagnames(t)
+            if sym != Symbol()
+                print(io, "\n", sym, " = ")
+                show(io, xi)
+            end
+            xi <<= 1
+        end
+    else
+        invoke(show, Tuple{IO,typeof(m),Type}, io, m, t)
+    end
+end
+
+@noinline flagset_argument_error(typename, x) =
+    throw(ArgumentError(string("invalid value for FlagSet $(typename): $x")))
+
+"""
+    @flagset FlagSetName[::BaseType] flag1[=x] flag2[=y]
+
+Create a `FlagSet{BaseType}` subtype with name `FlagSetName` and flags `flag1` and `flag2`
+with optional associated values of `x` and `y`, respectively, which must be positive
+powers of 2.
+Flag sets are implemented similarly to [`BitSet`](@ref), but its members are `Symbol`s.
+Eash instance of `FlagSetName` represents a subset of `Set([:flag1, :flag2])`, where
+- `FlagSetName(0) == FlagSetName()` represents the empty set `Set()`;
+- `FlagSetName(x) == FlagSetName(:flag1)` represents `Set([:flag1])`;
+- `FlagSetName(y) == FlagSetName(:flag2)` represents `Set([:flag2])`; and
+- `FlagSetName(x | y) == FlagSetName(:flag1, :flag2)` represents `Set([:flag1, :flag2])`.
+
+# Examples
+```jldoctest fontflags
+julia> @flagset FontFlags bold=1 italic=2 large=4
+
+julia> FontFlags(1)
+FontFlags(:bold)
+
+julia> FontFlags(:bold, :italic)
+FontFlags(:bold, :italic)
+
+julia> for flag in FontFlags(3); println(flag) end
+bold
+italic
+
+julia> FontFlags(3) | FontFlags(4)
+FontFlags(:bold, :italic, :large)
+```
+
+Values can also be specified inside a `begin` block, e.g.
+
+```julia
+@flagset FlagSetName begin
+    flag1
+    flag2
+end
+```
+
+`BaseType`, which defaults to [`UInt32`](@ref), must be a primitive subtype of `Integer`.
+Eash instance of `FlagSetName` can be converted to `BaseType`, where each bit set in the
+converted `BaseType` corresponds to a flag in the flag set. `read` and `write`
+perform these conversions automatically. In case the flagset is created with a non-default
+`BaseType`, `Integer(flagset)` will return the integer `flagset` with the type `BaseType`.
+
+To list flags of a flag set type or instance, use `flags`, e.g.
+
+```jldoctest fontflags
+julia> flags(FontFlags)
+(:bold, :italic, :large)
+
+julia> flags(FontFlags(3))
+(:bold, :italic)
+```
+"""
+macro flagset(T::Union{Symbol,Expr}, syms...)
+    if isempty(syms)
+        throw(ArgumentError("no arguments given for FlagSet $T"))
+    end
+    basetype = UInt32
+    if isa(T, Expr) && T.head === :(::) && length(T.args) == 2 && isa(T.args[1], Symbol)
+        typename = T.args[1]
+        basetype = Core.eval(__module__, T.args[2])
+        if !isa(basetype, DataType) || !(basetype <: Integer) || !isbitstype(basetype)
+            throw(ArgumentError(
+                "invalid base type for FlagSet $typename, ::$basetype; " *
+                "base type must be an unsigned integer primitive type",
+            ))
+        end
+    elseif isa(T, Symbol)
+        typename = T
+    else
+        throw(ArgumentError("invalid type expression for bit flag $T"))
+    end
+    len = sizeof(basetype) * 8
+    values = fill(Symbol(), len)
+    values_end = 0
+    nm = Dict{Symbol,basetype}()
+    i = one(basetype)
+    mask = zero(basetype)
+
+    if length(syms) == 1 && syms[1] isa Expr && syms[1].head === :block
+        syms = syms[1].args
+    end
+    for s in syms
+        s isa LineNumberNode && continue
+        if isa(s, Expr) &&
+           (s.head === :(=) || s.head === :kw) &&
+           length(s.args) == 2 &&
+           isa(s.args[1], Symbol)
+            i = Core.eval(__module__, s.args[2]) # allow exprs, e.g. uint128"1"
+            if !isa(i, Integer)
+                throw(ArgumentError(
+                    "invalid value for FlagSet $typename, $s; " *
+                    "values must be unsigned integers",
+                ))
+            end
+            if !ispow2(i)
+                throw(ArgumentError(
+                    "invalid value for FlagSet $typename, $s; " *
+                    "each value must be a positive power of 2",
+                ))
+            end
+            i = convert(basetype, i)
+            s = s.args[1]
+        elseif !(s isa Symbol) || s == Symbol()
+            throw(ArgumentError(string("invalid argument for FlagSet ", typename, ": ", s)))
+        end
+        if i <= zero(basetype)
+            throw(ArgumentError("overflow in value \"$s\" of FlagSet $typename"))
+        end
+        s = s::Symbol
+        if (i & mask) != 0
+            throw(ArgumentError("values for FlagSet $typename are not unique"))
+        end
+        if haskey(nm, s)
+            throw(ArgumentError("name \"$s\" in FlagSet $typename is not unique"))
+        end
+        nm[s] = i
+        nbit = trailing_zeros(i)
+        values[nbit+1] = s
+        mask |= i
+        values_end = max(values_end, nbit + 1)
+        i <<= 1
+    end
+    fnames = Tuple(values[1:values_end])
+    fnames_filtered = filter(!isequal(Symbol()), fnames)
+    blk = quote
+        # flagset definition
+        Base.@__doc__(
+            primitive type $(esc(typename)) <: FlagSet{$(basetype)} $(sizeof(basetype) * 8) end
+        )
+        function $(esc(typename))(x::Integer)
+            return bitcast($(esc(typename)), convert($(basetype), x) & $(mask))
+        end
+        function $(esc(typename))(sym::Symbol, syms::Symbol...)
+            xi::$(basetype) = get(FlagSets.namemap($(esc(typename))), sym) do
+                flagset_argument_error($(Expr(:quote, typename)), sym)
+            end
+            for sym ∈ syms
+                xi |= get(FlagSets.namemap($(esc(typename))), sym) do
+                    flagset_argument_error($(Expr(:quote, typename)), sym)
+                end
+            end
+            $(esc(typename))(xi)
+        end
+        $(esc(typename))() = $(esc(typename))($(zero(basetype)))
+        function $(esc(typename))(itr)
+            Base.isiterable(itr) || flagset_argument_error($(Expr(:quote, typename)), sym)
+            $(esc(typename))(itr...)
+        end
+
+        FlagSets.flagnames(::Type{$(esc(typename))}) = $(esc(fnames))
+        FlagSets.flags(::Type{$(esc(typename))}) = $(esc(fnames_filtered))
+        FlagSets.namemap(::Type{$(esc(typename))}) = $(esc(nm))
+        Base.typemin(x::Type{$(esc(typename))}) = $(esc(typename))($(zero(basetype)))
+        Base.typemax(x::Type{$(esc(typename))}) = $(esc(typename))($mask)
+    end
+    push!(blk.args, :nothing)
+    blk.head = :toplevel
+    return blk
+end
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,197 @@
+# This file is part of FlagSets.jl project, which is licensed under BDS-3-Clause.
+# See file LICENSE.md for more information.
+
+using FlagSets
+using Test, Serialization
+
+macro macrocall(ex)
+    @assert Meta.isexpr(ex, :macrocall)
+    ex.head = :call
+    for i = 2:length(ex.args)
+        ex.args[i] = QuoteNode(ex.args[i])
+    end
+    insert!(ex.args, 3, __module__)
+    return esc(ex)
+end
+
+@testset "Basic operation" begin
+    # Inline definition
+    @flagset Flag1 flag1a flag1b flag1c
+    @test Int(Flag1(:flag1a)) == 1
+    @test Int(Flag1(:flag1b)) == 2
+    @test Int(Flag1(:flag1c)) == 4
+    @test Flag1(1) == Flag1(:flag1a)
+    @test Flag1(2) == Flag1(:flag1b)
+    @test Flag1(4) == Flag1(:flag1c)
+    @test flags(Flag1) == (:flag1a, :flag1b, :flag1c)
+
+    # Block definition
+    @flagset Flag2 begin
+        flag2a
+        flag2b
+        flag2c
+    end
+    @test Int(Flag2(:flag2a)) == 1
+    @test Int(Flag2(:flag2b)) == 2
+    @test Int(Flag2(:flag2c)) == 4
+
+    # Explicit numbering, inline
+    @flagset Flag3 flag3a = 1 flag3b flag3c = 8
+    @test Int(Flag3(:flag3a)) == 1
+    @test Int(Flag3(:flag3b)) == 2
+    @test Int(Flag3(:flag3c)) == 8
+    @test Int(typemin(Flag3)) == 0
+    @test Int(typemax(Flag3)) == 11
+    @test flags(Flag3) == (:flag3a, :flag3b, :flag3c)
+
+    # Mask operations
+    @test Flag1(:flag1a) | Flag1(:flag1b) | Flag1(:flag1c) ==
+          Flag1(:flag1a, :flag1b, :flag1c)
+    @test Int(Flag1(7)) == 7
+    @test Flag1(7) == Flag1(:flag1a) | Flag1(:flag1b) | Flag1(:flag1c)
+    @test Flag1(7) & Flag1(:flag1a) == Flag1(:flag1a)
+    @test Flag1(7) ⊻ Flag1(:flag1a) == Flag1(:flag1b, :flag1c)
+    @test Int(Flag1(:flag1a)) < Int(Flag1(:flag1b)) < Int(Flag1(:flag1c))
+    @test Int(Flag1(:flag1a) | Flag1(:flag1b)) < Int(Flag1(:flag1c))
+end # testset
+
+@testset "Type properties" begin
+    # Default integer typing
+    @flagset Flag5 flag5a flag5b
+    @test typeof(Integer(Flag5(:flag5a))) == UInt32
+    @test typeof(Flag5) == DataType
+    @test typeof(Flag5(:flag5a)) <: Flag5 <: FlagSet <: AbstractSet
+    @test isbitstype(Flag5)
+    @test isbits(Flag5(:flag5a))
+
+    # Construct non-default
+    @flagset Flag6::UInt8 flag6a flag6b flag6c
+    @test typeof(Integer(Flag6(:flag6a))) == UInt8
+    @test UInt8(Flag6(:flag6a)) === 0x01
+    @test UInt16(Flag6(:flag6b)) === 0x0002
+    @test UInt128(Flag6(:flag6c)) === 0x00000000000000000000000000000004
+
+    # Explicit values of non-default types
+    @flagset Flag7::UInt8 flag7a = big"1" flag7b = UInt8(2)
+    @test Integer(Flag7(:flag7a)) === UInt8(1)
+    @test Integer(Flag7(:flag7b)) === UInt8(2)
+end # testset
+
+@testset "Error conditions" begin
+
+    @test_throws ArgumentError("no arguments given for FlagSet Foo") @macrocall(@flagset Foo)
+    @test_throws ArgumentError(
+        "invalid base type for FlagSet Foo, " *
+        "::Float64; base type must be an unsigned " *
+        "integer primitive type",
+    ) @macrocall(@flagset Foo::Float64 x = 1.0)
+
+    # Require uniqueness
+    @test_throws ArgumentError("values for FlagSet Foo are not unique") @macrocall(@flagset Foo x =
+        1 y = 1)
+    @test_throws ArgumentError("values for FlagSet Foo are not unique") @macrocall(@flagset Foo x =
+        2 y = 2)
+
+    # Explicit values must be powers of two
+    @test_throws ArgumentError(
+        "invalid value for FlagSet Foo, _three = 3; " *
+        "each value must be a positive power of 2",
+    ) @macrocall(@flagset Foo _three = 3)
+
+    # Values must be integers
+    @test_throws ArgumentError(
+        "invalid value for FlagSet Foo, _zero = \"zero\"; " *
+        "values must be unsigned integers",
+    ) @macrocall(@flagset Foo _zero = "zero")
+    @test_throws ArgumentError(
+        "invalid value for FlagSet Foo, _zero = '0'; " * "values must be unsigned integers",
+    ) @macrocall(@flagset Foo _zero = '0')
+    @test_throws ArgumentError(
+        "invalid value for FlagSet Foo, _zero = 0.5; " * "values must be unsigned integers",
+    ) @macrocall(@flagset Foo _zero = 0.5)
+
+    # Names must be valid identifiers
+    @test_throws ArgumentError("""invalid argument for FlagSet Foo: if x
+                                      1
+                                  else
+                                      2
+                                  end""") @macrocall(@flagset Foo x ? 1 : 2)
+    @test_throws ArgumentError("invalid argument for FlagSet Foo: 1 = 2") @macrocall(@flagset Foo 1 =
+        2)
+
+    # Disallow value overflow
+    @test_throws ArgumentError("overflow in value \"y\" of FlagSet Foo") @macrocall(@flagset Foo x =
+        (UInt32(1) << 31) y)
+
+end # testset
+
+@testset "Input/Output" begin
+    @flagset Flag9::UInt64 flag9a = 1 flag9b = 256
+    @flagset Flag10::UInt8 flag10a = 128 flag10b = 2
+    let io = IOBuffer()
+        write(io, Flag10(:flag10a, :flag10b))
+        write(io, Flag9(:flag9b))
+        write(io, Flag10(:flag10a))
+        seekstart(io)
+        @test read(io, Flag10) == Flag10(:flag10a, :flag10b)
+        @test read(io, Flag9) == Flag9(:flag9b)
+        @test read(io, Flag10) == Flag10(:flag10a)
+    end
+    let io = IOBuffer()
+        serialize(io, Flag9(:flag9a))
+        seekstart(io)
+        @test deserialize(io) === Flag9(:flag9a)
+    end
+end # testset
+
+# In submodule and not imported to test prefix printing
+module SubModule
+using ..FlagSets
+@flagset Bits::UInt8 one two four eight
+end
+
+@testset "String representations" begin
+    @flagset FilePerms::UInt8 READ = 4 WRITE = 2 EXEC = 1
+
+    @test string(FilePerms) == "FilePerms"
+    @test string(SubModule.Bits) == "Main.SubModule.Bits"
+    @test string(FilePerms()) == "FilePerms()"
+    @test string(SubModule.Bits(:one)) == "Main.SubModule.Bits(:one)"
+    @test repr("text/plain", FilePerms) == """FlagSet $(string(FilePerms)):
+                                              EXEC = 0x01
+                                              WRITE = 0x02
+                                              READ = 0x04"""
+    @test repr("text/plain", SubModule.Bits) == """FlagSet Main.SubModule.Bits:
+                                                   one = 0x01
+                                                   two = 0x02
+                                                   four = 0x04
+                                                   eight = 0x08"""
+    @test repr(FilePerms(:EXEC)) == "FilePerms(:EXEC)"
+    @test repr(SubModule.Bits(:one)) == "Main.SubModule.Bits(:one)"
+    @test repr(FilePerms(:EXEC, :READ)) == "FilePerms(:EXEC, :READ)"
+    @test repr(SubModule.Bits(:one, :eight)) == "Main.SubModule.Bits(:one, :eight)"
+    @test repr(FilePerms() | FilePerms(:READ)) == "FilePerms(:READ)"
+
+    let io = IOBuffer(), ioc = IOContext(io, :compact => true, :module => Main)
+        show(io, MIME"text/plain"(), FlagSet)
+        @test String(take!(io)) == "FlagSet"
+        show(ioc, MIME"text/plain"(), FlagSet)
+        @test String(take!(io)) == "FlagSet"
+
+        # Explicit :compact => false required for consistency across Julia versions
+        iof = IOContext(io, :compact => false, :module => Main)
+        show(iof, MIME"text/plain"(), Union{FilePerms,SubModule.Bits})
+        @test String(take!(io)) == "Union{FilePerms, Main.SubModule.Bits}"
+        show(ioc, MIME"text/plain"(), Union{FilePerms,SubModule.Bits})
+        @test String(take!(io)) == "Union{FilePerms, Bits}"
+
+        show(ioc, FilePerms())
+        @test String(take!(io)) == "FilePerms(0x00)"
+        show(ioc, SubModule.Bits(:one))
+        @test String(take!(io)) == "Bits(0x01)"
+        show(ioc, FilePerms(:EXEC, :READ))
+        @test String(take!(io)) == "FilePerms(0x05)"
+        show(ioc, SubModule.Bits(:one, :eight))
+        @test String(take!(io)) == "Bits(0x09)"
+    end # testset
+end
